4bb09f3af8c33e333d3e151a8e8a16705eeb3a38
Hannah <hannah.larkin8@gmail.com>
2026-03-04 16:10:22 -0700

    created hashmaps and generated implements

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -1,5 +1,65 @@
 package dataaccess;
 
-public class MemoryDataAccess {
+import model.AuthData;
+import model.GameData;
+import model.UserData;
 
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+
+public class MemoryDataAccess implements DataAccess {
+    HashMap<String, UserData> users = new HashMap<>();
+    HashMap<String, AuthData> auths = new HashMap<>();
+    HashMap<Integer, GameData> games = new HashMap<>();
+
+    @Override
+    public void clear() throws DataAccessException {
+
+    }
+
+    @Override
+    public void createUser(UserData user) throws DataAccessException {
+
+    }
+
+    @Override
+    public UserData getUser(String username) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void createAuth(AuthData authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public AuthData getAuth(String authToken) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public void deleteAuth(AuthData authToken) throws DataAccessException {
+
+    }
+
+    @Override
+    public void createGame(GameData gameID) throws DataAccessException {
+
+    }
+
+    @Override
+    public GameData getGame(int gameID) throws DataAccessException {
+        return null;
+    }
+
+    @Override
+    public Collection<GameData> listGames() throws DataAccessException {
+        return List.of();
+    }
+
+    @Override
+    public void updateGame(GameData gameID) throws DataAccessException {
+
+    }
 }
