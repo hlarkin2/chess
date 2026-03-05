9f0508dbb529a7bcb01c88cf9068687df5ff1e4d
Hannah <hannah.larkin8@gmail.com>
2026-03-04 17:12:34 -0700

    finished MemoryDataAccess

--- server/src/main/java/dataaccess/MemoryDataAccess.java
+++ server/src/main/java/dataaccess/MemoryDataAccess.java
@@ -15,51 +15,53 @@ public class MemoryDataAccess implements DataAccess {
 
     @Override
     public void clear() throws DataAccessException {
-
+        users.clear();
+        auths.clear();
+        games.clear();
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
-
+        users.put(user.username(), user);
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
-        return null;
+        return users.get(username);
     }
 
     @Override
     public void createAuth(AuthData authToken) throws DataAccessException {
-
+        auths.put(authToken.authToken(), authToken);
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
-        return null;
+        return auths.get(authToken);
     }
 
     @Override
     public void deleteAuth(AuthData authToken) throws DataAccessException {
-
+        auths.remove(authToken.authToken());
     }
 
     @Override
     public void createGame(GameData gameID) throws DataAccessException {
-
+        games.put(gameID.gameID(), gameID);
     }
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        return null;
+        return games.get(gameID);
     }
 
     @Override
     public Collection<GameData> listGames() throws DataAccessException {
-        return List.of();
+        return games.values();
     }
 
     @Override
     public void updateGame(GameData gameID) throws DataAccessException {
-
+        games.put(gameID.gameID(), gameID);
     }
 }
