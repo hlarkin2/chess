c56c3bb0fb40b6b57adb099e7ddde958e6ec11cf
Hannah <hannah.larkin8@gmail.com>
2026-03-11 22:36:19 -0600

    user and auth sql queries

--- server/src/main/java/dataaccess/DBDataAccess.java
+++ server/src/main/java/dataaccess/DBDataAccess.java
@@ -20,8 +20,8 @@ public class DBDataAccess implements DataAccess {
 
     private void submitConfig(String sql) throws DataAccessException {
         try (var conn = DatabaseManager.getConnection();
-             var ps = conn.prepareStatement(sql)) {
-            ps.executeUpdate();
+             var preparedStatement = conn.prepareStatement(sql)) {
+            preparedStatement.executeUpdate();
         } catch (SQLException e) {
             throw new DataAccessException("failed to configure the database", e);
         }
@@ -38,32 +38,105 @@ public class DBDataAccess implements DataAccess {
 
     @Override
     public void clear() throws DataAccessException {
+        String deleteGame = "DELETE FROM GameData;";
+        String deleteAuth = "DELETE FROM AuthData;";
+        String deleteUser = "DELETE FROM UserData;";
 
+        submitConfig(deleteGame);
+        submitConfig(deleteAuth);
+        submitConfig(deleteUser);
     }
 
     @Override
     public void createUser(UserData user) throws DataAccessException {
+        String username = user.username();
+        String password = user.password();
+        String email = user.email();
 
+        String salt = BCrypt.gensalt(12);
+        String hashedPass = BCrypt.hashpw(password, salt);
+
+        String insertUser = "INSERT INTO UserData (username, password, email) VALUES (?, ?, ?);";
+
+        try (var conn = DatabaseManager.getConnection();
+            var preparedStatement = conn.prepareStatement(insertUser)) {
+            preparedStatement.setString(1, username);
+            preparedStatement.setString(2, hashedPass);
+            preparedStatement.setString(3, email);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("error with creating user", e);
+        }
     }
 
     @Override
     public UserData getUser(String username) throws DataAccessException {
+        String getUser = "SELECT username, password, email FROM UserData WHERE username = ?;";
+
+        try (var conn = DatabaseManager.getConnection();
+            var preparedStatement = conn.prepareStatement(getUser)) {
+            preparedStatement.setString(1, username);
+            try (var returns = preparedStatement.executeQuery()) {
+                if (returns.next()) {
+                    String returnedUsername = returns.getString("username");
+                    String returnedPassword = returns.getString("password");
+                    String returnedEmail = returns.getString("email");
+                    return new UserData(returnedUsername, returnedPassword, returnedEmail);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error getting user", e);
+        }
         return null;
     }
 
     @Override
     public void createAuth(AuthData authToken) throws DataAccessException {
+        String username = authToken.username();
+        String insertAuth = "INSERT INTO AuthData (authToken, username) VALUES (?, ?);";
 
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(insertAuth)) {
+            preparedStatement.setString(1, authToken.authToken());
+            preparedStatement.setString(2, username);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("error with creating authToken", e);
+        }
     }
 
     @Override
     public AuthData getAuth(String authToken) throws DataAccessException {
+        String getAuth = "SELECT authToken, username FROM AuthData WHERE authToken = ?;";
+
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(getAuth)) {
+            preparedStatement.setString(1, authToken);
+            try (var returns = preparedStatement.executeQuery()) {
+                if (returns.next()) {
+                    String returnedToken = returns.getString("authToken");
+                    String returnedUsername = returns.getString("username");
+                    return new AuthData(returnedToken, returnedUsername);
+                }
+            }
+        } catch (SQLException e) {
+            throw new DataAccessException("error getting token", e);
+        }
         return null;
     }
 
     @Override
     public void deleteAuth(AuthData authToken) throws DataAccessException {
+        String token = authToken.authToken();
+        String delAuth = "DELETE FROM AuthData WHERE authToken = ?;";
 
+        try (var conn = DatabaseManager.getConnection();
+             var preparedStatement = conn.prepareStatement(delAuth)) {
+            preparedStatement.setString(1, token);
+            preparedStatement.executeUpdate();
+        } catch (SQLException e) {
+            throw new DataAccessException("error getting token", e);
+        }
     }
 
     @Override
