8529f9d00f15492d6f0f43e87fbbfbad9a4a4d88
Hannah <hannah.larkin8@gmail.com>
2026-03-11 20:09:31 -0600

    trying to create db

--- server/src/main/java/dataaccess/DBDataAccess.java
+++ server/src/main/java/dataaccess/DBDataAccess.java
@@ -1,4 +1,15 @@
 package dataaccess;
+import java.sql.*;
 
-public class DBDataAccess {
+public class DBDataAccess implements DataAccess {
+
+    public DBDataAccess() {
+        DatabaseManager.createDatabase();
+    }
+
+    private void configureDatabase() {
+        String createUser = "CREATE TABLE IF NOT EXISTS UserData (username VARCHAR(50) PRIMARY KEY, password VARCHAR(50), email VARCHAR(50))";
+        String createAuth = "CREATE TABLE IF NOT EXISTS AuthData (authToken VARCHAR(50) PRIMARY KEY, username VARCHAR(50))";
+        String createGame = "CREATE TABLE IF NOT EXISTS GameData (gameID INT PRIMARY KEY, whiteUsername VARCHAR(50), blackUsername VARCHAR(50), gameName VARCHAR(50), game TEXT)";
+    }
 }
