33773cdcdf2122a916cf6e8d7d7f394c9f3795a6
Hannah <hannah.larkin8@gmail.com>
2026-03-12 23:17:36 -0600

    working through tests

--- server/src/test/java/dataaccess/DatabaseAccessTests.java
+++ server/src/test/java/dataaccess/DatabaseAccessTests.java
@@ -1,5 +1,8 @@
 package dataaccess;
 
+import model.AuthData;
+import model.UserData;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -9,100 +12,119 @@ public class DatabaseAccessTests {
     @BeforeEach
     void setup() throws DataAccessException {
         dbAccess = new DBDataAccess();
+        dbAccess.clear();
     }
 
     @Test
-    void clearSuccess() {
-
+    void clearSuccess() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        dbAccess.createUser(user);
+        dbAccess.clear();
+        Assertions.assertNull(dbAccess.getUser(user.username()));
     }
 
     @Test
-    void createUserSuccess() {
-
+    void createUserSuccess() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        dbAccess.createUser(user);
+        UserData userName = dbAccess.getUser(user.username());
+        Assertions.assertNotNull(userName);
+        Assertions.assertEquals(user.username() ,userName.username());
     }
 
     @Test
-    void createUserFail() {
-
+    void createUserFail() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        dbAccess.createUser(user);
+        Assertions.assertThrows(DataAccessException.class, () -> {
+            dbAccess.createUser(user);
+        });
     }
 
     @Test
-    void getUserSuccess() {
-
+    void getUserSuccess() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        dbAccess.createUser(user);
+        UserData userName = dbAccess.getUser(user.username());
+        Assertions.assertNotNull(userName);
+        Assertions.assertEquals(user.username() ,userName.username());
     }
 
     @Test
-    void getUserFail() {
-
+    void getUserFail() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        Assertions.assertNull(dbAccess.getUser(user.username()));
     }
 
     @Test
-    void createAuthSuccess() {
-
+    void createAuthSuccess() throws DataAccessException {
+        UserData user = new UserData("amy", "hello", "email@gmail.com");
+        dbAccess.createUser(user);
+        AuthData token = dbAccess.createAuth(user);
     }
 
     @Test
-    void createAuthFail() {
-
-    }
-
-    @Test
-    void getAuthSuccess() {
-
-    }
-
-    @Test
-    void getAuthFail() {
-
-    }
-
-    @Test
-    void deleteAuthSuccess() {
-
-    }
-
-    @Test
-    void deleteAuthFail() {
-
-    }
-
-    @Test
-    void createGameSuccess() {
+    void createAuthFail() throws DataAccessException {
 
     }
 
     @Test
-    void createGameFail() {
+    void getAuthSuccess() throws DataAccessException {
 
     }
 
     @Test
-    void getGameSuccess() {
+    void getAuthFail() throws DataAccessException {
 
     }
 
     @Test
-    void getGameFail() {
+    void deleteAuthSuccess() throws DataAccessException {
 
     }
 
     @Test
-    void listGamesSuccess() {
+    void deleteAuthFail() throws DataAccessException {
 
     }
 
     @Test
-    void listGamesFail() {
+    void createGameSuccess() throws DataAccessException {
 
     }
 
     @Test
-    void updateGameSuccess() {
+    void createGameFail() throws DataAccessException {
 
     }
 
     @Test
-    void updateGameFail() {
+    void getGameSuccess() throws DataAccessException {
+
+    }
+
+    @Test
+    void getGameFail() throws DataAccessException {
+
+    }
+
+    @Test
+    void listGamesSuccess() throws DataAccessException {
+
+    }
+
+    @Test
+    void listGamesFail() throws DataAccessException {
+
+    }
+
+    @Test
+    void updateGameSuccess() throws DataAccessException {
+
+    }
+
+    @Test
+    void updateGameFail() throws DataAccessException {
 
     }
 
