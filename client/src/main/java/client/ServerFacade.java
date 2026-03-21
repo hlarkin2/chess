bd0ff6f06b6804aab3b31d04943aa990daf7ee34
Hannah <hannah.larkin8@gmail.com>
2026-03-21 17:30:14 -0600

    finished makeRequest

--- client/src/main/java/client/ServerFacade.java
+++ client/src/main/java/client/ServerFacade.java
@@ -8,47 +8,60 @@ import model.GameData;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URL;
 
 public class ServerFacade {
     private final String serverUrl;
+    private record ErrorMessage(String message) {}
 
     public ServerFacade(int port) {
         serverUrl = "http://localhost:" + port;
     }
 
     public AuthData register(UserData user) throws ResponseException {
-
+        var path = "/user";
+        return this.makeRequest("POST", path, user, AuthData.class, null);
     }
 
-    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass) throws ResponseException {
+    private <T> T makeRequest(String method, String path, Object request, Class<T> responseClass, String token) throws ResponseException {
         try {
-            URL url = (new URI(serverUrl + path)).toUrl();
-            httpURLConnection http = (HttpURLConnection) url.openConnection();
+            URL url = (new URI(serverUrl + path)).toURL();
+            HttpURLConnection http = (HttpURLConnection) url.openConnection();
             http.setRequestMethod(method);
             http.setDoOutput(true);
+            http.addRequestProperty("Content-Type", "application/json");
+            if (token != null) {
+                http.addRequestProperty("authorization", token);
+            }
 
             writeBody(request, http);
             http.connect();
             throwIfNotSuccessful(http);
             return readBody(http, responseClass);
         } catch (Exception e) {
-            throw new ResponseException(500, e.getMessage());
+            throw new ResponseException(e.getMessage(), e);
         }
     }
 
     private static void writeBody(Object request, HttpURLConnection http) throws IOException {
         if (request != null) {
-            http.addRequestProperty("Content Type", "application/json");;
+            String requestData = new Gson().toJson(request);
+            try (OutputStream requestBody = http.getOutputStream()) {
+                requestBody.write(requestData.getBytes());
+            }
         }
     }
 
     private void throwIfNotSuccessful(HttpURLConnection http) throws IOException, ResponseException {
         var status = http.getResponseCode();
         if (!isSuccessful(status)) {
-            throw new ResponseException(status, "failue: " + status);
+            InputStream errorStream = http.getErrorStream();
+            InputStreamReader reader = new InputStreamReader(errorStream);
+            ErrorMessage message = new Gson().fromJson(reader, ErrorMessage.class);
+            throw new ResponseException("Error " + status + ": " + message.message());
         }
     }
 
@@ -58,10 +71,14 @@ public class ServerFacade {
             try (InputStream responseBody = http.getInputStream()) {
                 InputStreamReader reader = new InputStreamReader(responseBody);
                 if (responseClass != null) {
-                    response = new Gson.fromJson(reader, responseClass);
+                    response = new Gson().fromJson(reader, responseClass);
                 }
             }
         }
         return response;
+    }
+
+    private boolean isSuccessful(int status) {
+        return status / 100 == 2;
     }
 }
