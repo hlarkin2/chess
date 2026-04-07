7c45fb62f11797bed5415830602d37bcb6f33a87
Hannah <hannah.larkin8@gmail.com>
2026-04-06 21:38:53 -0600

    fixed dependency errors

--- client/src/main/java/client/WebSocketCommunicator.java
+++ client/src/main/java/client/WebSocketCommunicator.java
@@ -1,4 +1,27 @@
 package client;
 
+import jakarta.websocket.*;
+import jakarta.websocket.ContainerProvider;
+import java.net.URI;
+
+@ClientEndpoint
 public class WebSocketCommunicator {
+    private ServerMessageObserver observer;
+    private Session session;
+
+    public WebSocketCommunicator(ServerMessageObserver observer, String serverUrl) throws Exception {
+        this.observer = observer;
+        URI uri = URI.create(serverUrl + "/ws");
+        WebSocketContainer container = ContainerProvider.getWebSocketContainer();
+        session = container.connectToServer(this, uri);
+    }
+
+    public void send(String message) {
+
+    }
+
+    @OnMessage
+    public void onMessage(String message) {
+
+    }
 }
